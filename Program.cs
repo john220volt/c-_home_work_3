298d250b28e8e01c99096d60c1cd678c43a17d92
Evgeni Suhinin <sewsk-boy@yandex.ru>
2023-06-30 22:44:26 +0300

    c#1

--- Program.cs
+++ Program.cs
@@ -1,8 +1,11 @@
-﻿Console.WriteLine("Введите число");
-string? str = Console.ReadLine();
+﻿System.Console.WriteLine("Введите число");
+string str = System.Console.ReadLine();
 
 if (str [0] == str [4])
 {
-    Console.WriteLine("Введенное число - палиндром");
+    System.Console.WriteLine("Введенное число - палиндром");
 }
-else (Console.WriteLine("Введенное число - не палиндром"));
+else 
+{
+    (System.Console.WriteLine("Введенное число - не палиндром"));
+}
